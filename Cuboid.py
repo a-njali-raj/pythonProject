46fda098ff29f594b7459d88f322b7077f0172ca
Anjali Raj <117928198+a-njali-raj@users.noreply.github.com>
2023-01-12 14:25:48 +0530

    Add files via upload

---
+++ Cuboid.py
@@ -0,0 +1,6 @@
+def area(l,b,h):
+    return 2*((l*b)+(b*h)+(h*l))
+def perimeter(l,b,h):
+    return(4*(l+b+h))
+#print(area(10,20,5))
+#print(perimeter(10,20,5))
---
+++ Sphere.py
@@ -0,0 +1,7 @@
+import math
+def area(rad):
+    return 4*math.pi*rad*rad
+def perimeter(rad):
+    return 2*math.pi*rad
+#print(area(10))
+#print(perimeter(10))
